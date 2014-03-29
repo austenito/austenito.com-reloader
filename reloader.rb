57bb680f41c725936e76cc0f544cb9f40e72b235
Austen Ito <austen.ito@gmail.com>
2014-03-29 14:08:25 -0400

    change from get to postage

--- reloader.rb
+++ reloader.rb
@@ -3,7 +3,7 @@ require 'yaml'
 
 class Reloader < Sinatra::Base
   config = YAML.load_file('config.yml')
-  get "/#{config["url"]}" do
+  post "/#{config["url"]}" do
     system("./#{config["script"]}")
     "Finished generating site"
   end
